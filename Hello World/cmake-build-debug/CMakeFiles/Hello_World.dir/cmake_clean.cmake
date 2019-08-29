9bdca94148e9d204ea5c5bc04a7323e62e98f4f4
Miles Lee <1789216340lhy@gmail.com>
2019-08-29 15:04:46 -0500

    加入新的Hello World

---
+++ Hello World/cmake-build-debug/CMakeFiles/Hello_World.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Hello_World.dir/main.cpp.o"
+  "Hello_World.pdb"
+  "Hello_World"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Hello_World.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

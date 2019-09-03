a731de0f560860d76698569b25895b604de5ee7d
Miles Lee <1789216340lhy@gmail.com>
2019-09-03 17:16:52 -0500

    加入老吴的没法运行的程序

---
+++ Wu/cmake-build-debug/CMakeFiles/Wu.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Wu.dir/main.cpp.o"
+  "Wu.pdb"
+  "Wu"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Wu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

fd687933a3c0b27de6132939efbb91ebbfa74577
Miles Lee <1789216340lhy@gmail.com>
2019-09-04 16:10:32 -0500

    按照编程规范重构

--- lab03a/Passer_Rating_Calculation.cpp
+++ lab03a/Passer_Rating_Calculation.cpp
@@ -8,40 +8,40 @@ using namespace std;
 
 int  main()
 {
-  float completions;
-  cout << "Please enter the number of completions: ";
+  double completions;
+  cout << "Enter the number of completions: ";
   cin >> completions;
 
-  float attempts;
-  cout << "Please enter the number of attempts: ";
+  double attempts;
+  cout << "Enter the number of attempts: ";
   cin >> attempts;
 
-  float yards;
-  cout << "Please enter the number of yards: ";
+  double yards;
+  cout << "Enter the number of yards: ";
   cin >> yards;
 
-  float touchdowns;
-  cout << "Please enter the number of touchdowns: ";
+  double touchdowns;
+  cout << "Enter the number of touchdowns: ";
   cin >> touchdowns;
 
-  float interceptions;
-  cout << "Please enter the number of interceptions: ";
+  double interceptions;
+  cout << "Enter the number of interceptions: ";
   cin >> interceptions;
 
-  float C;
-  C = ((completions / attempts) * 100.0 - 30.0) / 20.0;
+  double scaled_completions;
+  scaled_completions = ((completions / attempts) * 100.0 - 30.0) / 20.0;
 
-  float Y;
-  Y = ((yards / attempts) - 3.0) / 4.0;
+  double scaled_yards;
+  scaled_yards = ((yards / attempts) - 3.0) / 4.0;
 
-  float T;
-  T = touchdowns / attempts * 20.0;
+  double scaled_touchdowns;
+  scaled_touchdowns = touchdowns / attempts * 20.0;
 
-  float I;
-  I = 2.375 - 25.0 * (interceptions / attempts);
+  double scaled_interceptions;
+  scaled_interceptions = 2.375 - 25.0 * (interceptions / attempts);
 
-  float rating;
-  rating = ((C + Y + T + I) / 6.0) * 100.0;
+  double rating;
+  rating = ((scaled_completions + scaled_yards + scaled_touchdowns + scaled_interceptions) / 6.0) * 100.0;
   cout << "The player's rating is: " << rating << endl;
 
   return 0;

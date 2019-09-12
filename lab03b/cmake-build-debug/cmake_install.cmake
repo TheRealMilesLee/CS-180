36e1b1e4f3ff177c9f15a438c1b5eb63d379d545
Lee <38075314+MarkHooland@users.noreply.github.com>
2019-09-11 23:21:56 -0500

    加入运行文件

--- lab03b/cmake-build-debug/cmake_install.cmake
+++ lab03b/cmake-build-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: /Users/leemiles/Developer/Truman_CS180/Truman-CS180-Course/lab03b
+# Install script for directory: D:/Truman-CS180-Course/lab03b
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/usr/local")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/lab03b")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/leemiles/Developer/Truman_CS180/Truman-CS180-Course/lab03b/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "D:/Truman-CS180-Course/lab03b/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")

6468808fa5ef9a1195dc6c2edca9c6dcd6c1eba7
Lee <38075314+MarkHooland@users.noreply.github.com>
2019-09-17 16:15:47 -0500

    更新运行文件

--- lab05/Decisions.cpp
+++ lab05/Decisions.cpp
@@ -1,6 +1,6 @@
 //Hengyi Li
-//This Program is to deside the phone plan
-//This Program finished by Hengyi Li on 10：40 AM, 17 Sep, 2019
+//This Program is to decide the phone plan
+//This Program finished by Hengyi Li on 4：14 PM, 17 Sep, 2019
 //Copyright @ 2019 Hengyi Li. All rights reserved.
 
 #include <iostream>
@@ -8,13 +8,16 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
   double data_usage;
   double base_cost_a = 39.99;
   double base_cost_b = 59.99;
   double base_cost_c = 69.99;
   const unsigned LABEL_WIDTH = 13;
   const unsigned PRECISION = 2;
+  const double ADDITIONAL_DATA_COSTS_A = 9.99;
+  const double ADDITIONAL_DATA_COSTS_B = 4.99;
   string input_choice;
 
   cout << "Choose the plan (A, B ,Or C): " << endl;
@@ -24,27 +27,26 @@ int main() {
   {
     cout << "How many gigs of data did you use? " << endl;
     cin >> data_usage;
-    unsigned plan_a;
+    double plan_a;
     double invoice_total_a;
+    cout << "Base Cost     " << setw(LABEL_WIDTH) << setprecision(PRECISION) << fixed << base_cost_a << endl;
 
-
-    cout << "Base Cost    " << setw(LABEL_WIDTH) << setprecision(PRECISION) << fixed << base_cost_a << endl;
-    if (data_usage > 4)
+    if (data_usage > 4.0)
     {
-      plan_a = (data_usage - 4.0) * 9.99;
-      cout << "Data Cost    " << setw(LABEL_WIDTH) <<  plan_a << endl;
+      plan_a = (data_usage - 4.0) * ADDITIONAL_DATA_COSTS_A;
+      cout << "Data Cost     " << setw(LABEL_WIDTH) << setprecision(PRECISION) << fixed << plan_a << endl;
       invoice_total_a = base_cost_a + plan_a;
-      cout << "Invoice Total" << setw(LABEL_WIDTH) << invoice_total_a << endl;
+      cout << "Invoice Total " << setw(LABEL_WIDTH) << invoice_total_a << endl;
     }
     else
     {
-      cout << "Data Cost    " << setw(LABEL_WIDTH) << "0.00" << endl;
-      invoice_total_a = base_cost;
-      cout << "Invoice Total" << setw(LABEL_WIDTH) << invoice_total_a << endl;
+      cout << "Data Cost     " << setw(LABEL_WIDTH) << setprecision(PRECISION) << fixed << "0.00" << endl;
+      invoice_total_a = base_cost_a;
+      cout << "Invoice Total " << setw(LABEL_WIDTH) << invoice_total_a << endl;
     }
     
-
     return 0;
+
   }
 
   if (input_choice == "B")
@@ -53,22 +55,27 @@ int main() {
     cin >> data_usage;
     double plan_b;
     double invoice_total_b;
-    if (data_usage > 8)
+
+    if (data_usage > 8.0)
     {
-      plan_b = (data_usage - 8.0) * 4.99;
+      plan_b = (data_usage - 8.0) * ADDITIONAL_DATA_COSTS_B;
       invoice_total_b = base_cost_b + plan_b;;
-      cout << "Base Cost" << setw(LABEL_WIDTH) << setprecision(PRECISION) << fixed << base_cost_b << endl;
-      cout << "Data Cost" << setw(LABEL_WIDTH) << plan_b << endl;
-      cout << "Invoice Data" << setw(LABEL_WIDTH) << invoice_total_b << endl;
+      cout << "Base Cost    " << setw(LABEL_WIDTH) << setprecision(PRECISION) << fixed << base_cost_b << endl;
+      cout << "Data Cost    " << setw(LABEL_WIDTH) << plan_b << endl;
+      cout << "Invoice Data " << setw(LABEL_WIDTH) << invoice_total_b << endl;
     }
     else
     {
+
       invoice_total_b = base_cost_b;
-      cout << "Base Cost" << setw(LABEL_WIDTH) << setprecision(PRECISION) << fixed << base_cost_b << endl;
-      cout << "Data Cost" << setw(LABEL_WIDTH) << "0" << endl;
-      cout << "Invoice Data" << setw(LABEL_WIDTH) << invoice_total_b << endl;
+      cout << "Base Cost    " << setw(LABEL_WIDTH) << setprecision(PRECISION) << fixed << base_cost_b << endl;
+      cout << "Data Cost    " << setw(LABEL_WIDTH) << "0.00" << endl;
+      cout << "Invoice Data " << setw(LABEL_WIDTH) << invoice_total_b << endl;
+
     }
+
     return 0;
+
   }
 
   if (input_choice == "C")
@@ -77,12 +84,15 @@ int main() {
     cin >> data_usage;
     double plan_c;
     double invoice_total_c;
-    plan_c = 0;
-    invoice_total_c = base_cost_c + plan_c + 30;
-    cout << "Base Cost" << setw(LABEL_WIDTH) << setprecision(PRECISION) << fixed << base_cost_c << endl;
-    cout << "Data Cost" << setw(LABEL_WIDTH) << plan_c << endl;
-    cout << "Invoice Data" << setw(LABEL_WIDTH) << invoice_total_c << endl;
+    invoice_total_c = base_cost_c;
+    cout << "Base Cost    " << setw(LABEL_WIDTH) << setprecision(PRECISION) << fixed << base_cost_c << endl;
+    cout << "Data Cost    " << setw(LABEL_WIDTH) << "0.00" << endl;
+    cout << "Invoice Data " << setw(LABEL_WIDTH) << invoice_total_c << endl;
+
     return 0;
+
   }
+
   return 0;
+
 }
--- lab05/cmake-build-debug/CMakeFiles/lab05.dir/DependInfo.cmake
+++ lab05/cmake-build-debug/CMakeFiles/lab05.dir/DependInfo.cmake
@@ -4,9 +4,9 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/Users/leemiles/Developer/Truman_CS180/Truman-CS180-Course/lab05/Decisions.cpp" "/Users/leemiles/Developer/Truman_CS180/Truman-CS180-Course/lab05/cmake-build-debug/CMakeFiles/lab05.dir/Decisions.cpp.o"
+  "D:/Truman-CS180-Course/lab05/Decisions.cpp" "D:/Truman-CS180-Course/lab05/cmake-build-debug/CMakeFiles/lab05.dir/Decisions.cpp.obj"
   )
-set(CMAKE_CXX_COMPILER_ID "AppleClang")
+set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH

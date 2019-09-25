5e4990f83f13d9688358ed33fdc650c72c678cd8
Miles Lee <1789216340lhy@gmail.com>
2019-09-25 10:40:54 -0500

    Update CMakeOutput.log

--- lab06/cmake-build-debug/CMakeFiles/lab06.dir/cmake_clean.cmake
+++ lab06/cmake-build-debug/CMakeFiles/lab06.dir/cmake_clean.cmake
@@ -1,9 +1,7 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/lab06.dir/More_Phone_Plan.cpp.obj"
+  "CMakeFiles/lab06.dir/More_Phone_Plan.cpp.o"
   "lab06.pdb"
-  "lab06.exe"
-  "lab06.exe.manifest"
-  "liblab06.dll.a"
+  "lab06"
 )
 
 # Per-language clean rules from dependency scanning.

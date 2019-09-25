a40b090c8dcd703937a6de6d5a7e8ab676e7cace
Lee <38075314+MarkHooland@users.noreply.github.com>
2019-09-25 10:31:21 -0500

    lab06完成

--- lab06/main.cpp
+++ lab06/main.cpp
@@ -17,76 +17,81 @@ int main()
   double base_cost_a = 39.99;
   double base_cost_b = 59.99;
   double base_cost_c = 69.99;
-  string input_choice;
 
-  cout << "Choose the plan (A, B ,Or C) or Q to quit: " << endl;
-  cin >> input_choice;
-
-    while (input_choice == "A")
+  for (string input_choice; input_choice != "Q";)
     {
-      double data_usage;
-      cout << "How many gigs of data did you use? ";
-      cin >> data_usage;
-
-      while (data_usage < 0)
+      cout << "Choose the plan (A, B ,Or C) or Q to quit: ";
+      cin >> input_choice;
+      if (input_choice == "A" || "B" || "C" || "Q")
       {
-        cout << "Data usage cannot be negative. Please re-enter: ";
-        cin >> data_usage;
+        if (input_choice == "A")
+        {
+          double data_usage;
+          cout << "How many gigs of data did you use? ";
+          cin >> data_usage;
+
+          while (data_usage < 0)
+          {
+            cout << "Data usage cannot be negative. Please re-enter: ";
+            cin >> data_usage;
+          }
+
+          double plan_a;
+          double invoice_total_a;
+
+          if (data_usage > MAX_DATA_IN_PLAN_A)
+          {
+            plan_a = (data_usage - MAX_DATA_IN_PLAN_A) * ADDITIONAL_DATA_COSTS_A;
+            invoice_total_a = base_cost_a + plan_a;
+            cout << "Invoice Total " << invoice_total_a << endl;
+          }
+          else
+          {
+            invoice_total_a = base_cost_a;
+            cout << "Invoice Total " << invoice_total_a << endl;
+          }
+        }
+        else if (input_choice == "B")
+        {
+          double data_usage;
+
+          cout << "How many gigs of data did you use? ";
+          cin >> data_usage;
+
+          while (data_usage <= 0)
+          {
+            cout << "Data usage cannot be negative. Please re-enter: ";
+            cin >> data_usage;
+          }
+
+          double plan_b;
+          double invoice_total_b;
+
+          if (data_usage > MAX_DATA_IN_PLAN_B)
+          {
+            plan_b = (data_usage - MAX_DATA_IN_PLAN_B) * ADDITIONAL_DATA_COSTS_B;
+            invoice_total_b = base_cost_b + plan_b;
+            cout << "Invoice total " << invoice_total_b << endl;
+          }
+          else
+          {
+            invoice_total_b = base_cost_b;
+            cout << "Invoice total " << invoice_total_b << endl;
+          }
+
+        }
+        else if (input_choice == "C")
+        {
+          double invoice_total_c;
+          invoice_total_c = base_cost_c;
+          cout << "Invoice total " << invoice_total_c << endl;
+        }
+        else
+        {
+          cout << "Invaild Choice." << endl;
+        }
       }
 
-      double plan_a;
-      double invoice_total_a;
-
-      if (data_usage > MAX_DATA_IN_PLAN_A)
-      {
-        plan_a = (data_usage - MAX_DATA_IN_PLAN_A) * ADDITIONAL_DATA_COSTS_A;
-        invoice_total_a = base_cost_a + plan_a;
-        cout << "Invoice Total " << invoice_total_a << endl;
-      }
-      else
-      {
-        invoice_total_a = base_cost_a;
-        cout << "Invoice Total " << invoice_total_a << endl;
-      }
-      break;
     }
-
-    while (input_choice == "B")
-    {
-      double data_usage;
-
-      cout << "How many gigs of data did you use? " << endl;
-      cin >> data_usage;
-
-      while (data_usage <= 0)
-      {
-        cout << "Data usage cannot be negative. Please re-enter: ";
-        cin >> data_usage;
-      }
-
-      double plan_b;
-      double invoice_total_b;
-
-      if (data_usage > MAX_DATA_IN_PLAN_B)
-      {
-        plan_b = (data_usage - MAX_DATA_IN_PLAN_B) * ADDITIONAL_DATA_COSTS_B;
-        invoice_total_b = base_cost_b + plan_b;
-        cout << "Invoice total " << invoice_total_b << endl;
-      }
-      else
-      {
-        invoice_total_b = base_cost_b;
-        cout << "Invoice total " << invoice_total_b << endl;
-      }
-
-    }
-
-    while (input_choice == "C")
-    {
-      double invoice_total_c;
-      invoice_total_c = base_cost_c;
-      cout << "Invoice total " << invoice_total_c << endl;
-    }
-  return 0;
+    return 0;
 }
-

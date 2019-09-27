fc421a136d550cec1463ebd0e3eddee9cf7ba00a
Miles Lee <1789216340lhy@gmail.com>
2019-09-26 20:27:52 -0500

    更新格式

--- lab06/More_Phone_Plan.cpp
+++ lab06/More_Phone_Plan.cpp
@@ -1,6 +1,6 @@
 //Hengyi Li
 //This Program is to decide the phone plan
-//This Program finished by Hengyi Li on 12：41 PM, 25 Sep, 2019
+//This Program finished by Hengyi Li on 8：22 PM, 26 Sep, 2019
 //Copyright @ 2019 Hengyi Li. All rights reserved.
 
 #include <iostream>
@@ -17,77 +17,86 @@ int main()
   const unsigned PRECISION = 2;
   bool judge;
   for (string input_choice; input_choice != "Q"; judge = false)
+  {
+    cout << "Choose the plan (A, B ,Or C) or Q to quit: ";
+    cin >> input_choice;
+    if (input_choice == "A" || "B" || "C" || "Q")
     {
-      cout << "Choose the plan (A, B ,Or C) or Q to quit: ";
-      cin >> input_choice;
-      if (input_choice == "A" || "B" || "C" || "Q")
+      if (input_choice == "A")
       {
-        if (input_choice == "A")
+        double data_usage;
+        cout << "How many gigs of data did you use? ";
+        cin >> data_usage;
+        while (data_usage < 0)
         {
-          double data_usage;
-          cout << "How many gigs of data did you use? ";
+          cout << "Data usage cannot be negative. Please re-enter: ";
           cin >> data_usage;
-          while (data_usage < 0)
-          {
-            cout << "Data usage cannot be negative. Please re-enter: ";
-            cin >> data_usage;
-          }
-          double plan_a;
-          double invoice_total_a;
-          double base_cost_a = 39.99;
-          if (data_usage > MAX_DATA_IN_PLAN_A)
-          {
-            plan_a = (data_usage - MAX_DATA_IN_PLAN_A) * ADDITIONAL_DATA_COSTS_A;
-            invoice_total_a = base_cost_a + plan_a;
-            cout << "Invoice Total " << setprecision(PRECISION) << fixed << invoice_total_a << endl;
-          }
-          else
-          {
-            invoice_total_a = base_cost_a;
-            cout << "Invoice Total " << setprecision(PRECISION) << fixed << invoice_total_a << endl;
-          }
         }
-        else if (input_choice == "B")
+        double plan_a;
+        double invoice_total_a;
+        double base_cost_a = 39.99;
+        if (data_usage > MAX_DATA_IN_PLAN_A)
         {
-          double data_usage;
-          cout << "How many gigs of data did you use? ";
-          cin >> data_usage;
-          while (data_usage <= 0)
-          {
-            cout << "Data usage cannot be negative. Please re-enter: ";
-            cin >> data_usage;
-          }
-          double plan_b;
-          double invoice_total_b;
-          double base_cost_b = 59.99;
-          if (data_usage > MAX_DATA_IN_PLAN_B)
-          {
-            plan_b = (data_usage - MAX_DATA_IN_PLAN_B) * ADDITIONAL_DATA_COSTS_B;
-            invoice_total_b = base_cost_b + plan_b;
-            cout << "Invoice total " << setprecision(PRECISION) << fixed << invoice_total_b << endl;
-          }
-          else
-          {
-            invoice_total_b = base_cost_b;
-            cout << "Invoice total " << setprecision(PRECISION) << fixed << invoice_total_b << endl;
-          }
-        }
-        else if (input_choice == "C")
-        {
-          double base_cost_c = 69.99;
-          double invoice_total_c;
-          invoice_total_c = base_cost_c;
-          cout << "Invoice total " << setprecision(PRECISION) << fixed << invoice_total_c << endl;
-        }
-        else if (input_choice == "Q")
-        {
-          judge = true;
+          plan_a =
+              (data_usage - MAX_DATA_IN_PLAN_A) * ADDITIONAL_DATA_COSTS_A;
+          invoice_total_a = base_cost_a + plan_a;
+          cout << "Invoice Total " << setprecision(PRECISION) << fixed
+               << invoice_total_a << endl << endl;
         }
         else
         {
-          cout << "Invaild Choice." << endl;
+          invoice_total_a = base_cost_a;
+          cout << "Invoice Total " << setprecision(PRECISION) << fixed
+               << invoice_total_a << endl << endl;
         }
       }
+      else if (input_choice == "B")
+      {
+        double data_usage;
+        cout << "How many gigs of data did you use? ";
+        cin >> data_usage;
+        while (data_usage <= 0)
+        {
+          cout << "Data usage cannot be negative. Please re-enter: ";
+          cin >> data_usage;
+        }
+        double plan_b;
+        double invoice_total_b;
+        double base_cost_b = 59.99;
+        if (data_usage > MAX_DATA_IN_PLAN_B)
+        {
+          plan_b =
+              (data_usage - MAX_DATA_IN_PLAN_B) * ADDITIONAL_DATA_COSTS_B;
+          invoice_total_b = base_cost_b + plan_b;
+          cout << "Invoice total " << setprecision(PRECISION) << fixed
+               << invoice_total_b << endl << endl;
+        }
+        else
+        {
+          invoice_total_b = base_cost_b;
+          cout << "Invoice total " << setprecision(PRECISION) << fixed
+               << invoice_total_b << endl << endl;
+        }
+      }
+      else if (input_choice == "C")
+      {
+        double base_cost_c = 69.99;
+        double invoice_total_c;
+        invoice_total_c = base_cost_c;
+        cout << "Invoice total " << setprecision(PRECISION) << fixed
+             << invoice_total_c << endl << endl;
+      }
+      else if (input_choice == "Q")
+      {
+        judge = true;
+      }
+      else
+      {
+        cout << "Invaild Choice." << endl;
+      }
     }
-    return 0;
+  }
+
+  return 0;
+
 }

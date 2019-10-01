48cd1e71f8c7f456dd5ff4aec41b21720c611cdb
Lee <38075314+MarkHooland@users.noreply.github.com>
2019-10-01 13:06:23 -0500

    更新lab07

---
+++ lab07/cmake-build-debug/CMakeFiles/lab07.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab07.dir/main.cpp.obj"
+  "lab07.exe"
+  "lab07.exe.manifest"
+  "lab07.pdb"
+  "liblab07.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab07.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lab07/main.cpp
@@ -0,0 +1,61 @@
+//Hengyi Li
+//This Program is to decide the phone plan
+//This Program finished by Hengyi Li on 8：22 PM, 26 Sep, 2019
+//Copyright @ 2019 Hengyi Li. All rights reserved.
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <istream>
+
+using namespace std;
+
+int main()
+{
+  ifstream in_file;
+  in_file.open("scores.txt");
+
+  unsigned number_of_scores;
+  in_file >> number_of_scores;
+  cout << "The number of scores is: " << number_of_scores << endl;
+
+  string dummy;
+  getline(in_file, dummy);
+
+  string line;
+  while (getline(in_file, line))
+  {
+    string name_of_user;
+    getline(in_file, name_of_user);
+    for (unsigned loop_times; loop_times <= number_of_scores; loop_times++)
+    {
+      string numbers;
+      getline(in_file, numbers);
+    }
+    unsigned numbers;
+    double average_score;
+    average_score = (numbers / number_of_scores);
+    double highest_score;
+
+    double lowest_score;
+
+    double letter_grades;
+    if (average_score >= 90)
+    {
+      cout << name_of_user << average_score << highest_score  << lowest_score << "A" << endl;
+    }
+    else if (average_score >=80 && average_score < 90)
+    {
+      cout << name_of_user << average_score << highest_score  << lowest_score << "B" << endl;
+    }
+    else if (average_score >=70 && average_score < 80)
+    {
+      cout << name_of_user << average_score << highest_score  << lowest_score << "C" << endl;
+      else
+      {
+        cout << name_of_user << average_score << highest_score  << lowest_score << "D" << endl;
+      }
+
+      in_file.close();
+      return 0;
+    }

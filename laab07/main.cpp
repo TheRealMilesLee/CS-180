b141bcd017b3dead97c02e726500c414f4fb416f
Miles Lee <1789216340lhy@gmail.com>
2019-10-01 11:49:22 -0500

    update lab07

--- laab07/main.cpp
+++ laab07/main.cpp
@@ -15,6 +15,13 @@ int main()
   ifstream in_file;
   in_file.open("scores.txt");
 
+  unsigned number_of_scores;
+  in_file >> number_of_scores;
+  cout << "The number of scores is: " << number_of_scores << endl;
+
+  string dummy;
+  getline(in_file, dummy);
+
   string line;
   while (getline(in_file, line))
   {

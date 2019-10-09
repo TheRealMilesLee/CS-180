3743670fe17534813c56da4fcf98b5110761db0b
Miles Lee <1789216340lhy@gmail.com>
2019-10-08 22:07:24 -0500

    更新函数的使用实例

--- An_Example_For_Functions/functions_example.cpp
+++ An_Example_For_Functions/functions_example.cpp
@@ -1,21 +1,23 @@
-// A program to illustrate a function call, from Gaddis 6-2
+//函数使用的实例
 #include <iostream>
+
 using namespace std;
-/**
-display a simple message on the screen
-*/
- void display_message();
+//函数声明
+void display_message();
+//主程序
 int main()
 {
 cout << "Hello from main" << endl;
 for (unsigned count = 0; count < 5; count++)
 {
-display_message();
+    //函数调用
+    display_message();
 }
 cout << "Back in main again" << endl;
 return 0;
 }
 
+//函数的解释
 void display_message()
 {
 cout << "Hello from display_message" << endl;

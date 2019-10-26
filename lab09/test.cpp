4dcc8acb017df9d9a1297be7b49bc1ecf90c7661
Miles Lee <1789216340lhy@gmail.com>
2019-10-26 16:56:17 -0500

    更新测试文件支持

--- lab09/test.cpp
+++ lab09/test.cpp
@@ -2,26 +2,64 @@
 #include<fstream>
 
 using namespace std;
+const unsigned COLO_SIZE = 3;
+const unsigned ROW_SIZE = 3;
+
+string get_input_file(string& in_file);
+void print_square();
+void read_square_from_file(unsigned square[COLO_SIZE][ROW_SIZE], ifstream& file);
 
 int main()
 {
-    unsigned SIZE = 10;
-    unsigned numbers[SIZE];
-    unsigned count;
-    ofstream output_file;
-
-    for (count = 0; count < SIZE; count++)
-    {
-        numbers[count] = count;
-    }
-
-    output_file.open("1.txt");
-
-    for (count = 0; count < SIZE; count++)
-    {
-        output_file << numbers[count] << " | "  << endl;
-    }
-
-    output_file.close();
+    string filename;
+    get_input_file(filename);
+    print_square();
     return 0;
+}
+string get_input_file(string& in_file)
+{
+  ifstream file;
+  string filename;
+  cout << "Enter a filename for processing: ";
+  cin >> filename;
+  bool done = false;
+  while (!done)
+  {
+    file.open(filename);
+    if (file.fail())
+    {
+      cout << "Could not open foobar.txt. Please try again. " << endl;
+      cout << "Enter a filename for processing: ";
+      cin >> filename;
+    }
+    else
+    {
+        
+      return read_square_from_file(square,filename);
+    }
+  }
+}
+void print_square()
+{
+  bool done = false;
+  while (!done)
+  {
+    string filename;
+
+    for (unsigned count = 0; count < 3; count++)
+    {
+        cout << "+---+---+---+" << endl << "|" << read_square_from_file(square, filename) << "|" << endl;
+    }
+  }
+}
+void read_square_from_file(unsigned square[COLO_SIZE][ROW_SIZE], ifstream& file)
+{
+  for (unsigned row = 0; row < COLO_SIZE; row++)
+  {
+    for (unsigned col = 0; col < ROW_SIZE; col++)
+    {
+      file >> square[row][col];
+    }
+  }
+  file.close();
 }

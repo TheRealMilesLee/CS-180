78084555782acd3f742d214b11dd48fb48609b1b
Lee <38075314+MarkHooland@users.noreply.github.com>
2019-11-05 11:10:57 -0600

    更新动态分配问题

---
+++ lab11/dyamic.cpp
@@ -0,0 +1,17 @@
+//Hengyi Li
+//This Program is for show a Dynamic Array
+//This Program Created by Hengyi Li on 11:07 AM, Nov 5, 2019
+//This Program has been done by Hengyi Li on 1:07 AM, Nov , 2019.
+//Copyright @ 2019 Hengyi Li. All rights reserved.
+
+#include <random>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+
+  return 0;
+}

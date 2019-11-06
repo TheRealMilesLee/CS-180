ede835c833d384720ab0d51aed18861491e5f838
Miles Lee <1789216340lhy@gmail.com>
2019-11-05 19:37:40 -0600

    更新dyamic

--- lab11/dyamic.cpp
+++ lab11/dyamic.cpp
@@ -17,132 +17,95 @@ using namespace std;
  * @param values_array get the array that reads from the file
  * @param value_count get how many numbers that put in the array
  */
-void sort_array(*values, size_t value_count);
-
-/**
- * This function is used to copy aray to the vector
- * @param values_array get the values that in the array
- * @param values_vector initialize a vector that can store the array
- * @param value_count get how many numbers in the array
- */
-void copy_array_to_vector(vector<int> &values_vector, unsigned value_count);
-
-/**
- * This function is used to do a binary search in the vector.
- * @param values_vector is the searching place
- * @param value is the numbers that from the array
- * @return the searching result
- */
-size_t binary_search_vector(const vector<int> &values_vector);
+void sort_array(int values_of_array[], unsigned value_count);
 
 /**
  * this function is to get the random number in range.
- * @param low is the smallest number that the user type in
- * @param high is the biggest number that the user type in
+ * @param small is the smallest number that the user type in
+ * @param biggest is the biggest number that the user type in
  * @return the random value that can puts in the array
  */
-size_t get_rand_in_range(unsigned low, unsigned high);
+unsigned get_rand_in_range(unsigned small, unsigned biggest);
 
 /**
- * This function is to get the 
- * @return
+ * This function is to generate the array
+ * @param values_of_array
+ * @param user_allocated_size
  */
-size_t get_the_size_of_array();
+ void generate_the_array(int values_of_array[], unsigned
+ user_allocated_size);
+
+
+ void out_put_the_array(int values_of_array[], unsigned user_allocated_size);
+
+
+ void median_of_array(int values_of_array[], unsigned user_allocated_size);
 
 int main()
 {
-  vector<int> values_vector; // a vector of values
+  unsigned user_allocated_size;
+  cout <<"Enter the number of values: ";
+  cin >> user_allocated_size;
+  int* values = new int[user_allocated_size];
 
 
+  generate_the_array(values, user_allocated_size);
+  sort_array(values, user_allocated_size);
+  out_put_the_array(values, user_allocated_size);
+  median_of_array(values, user_allocated_size);
+  return 0;
+}
 
-  size_t value_count = 0;
+// function definitions here
+void sort_array(int values_of_array[], unsigned value_count)
+{
+  for (size_t pass_indx = value_count - 1; pass_indx > 0; pass_indx--)
+  {
+    for (size_t compare_indx = 0; compare_indx < pass_indx; compare_indx++)
+    {
+      if (values_of_array[compare_indx] > values_of_array[compare_indx + 1])
+      {
+        swap(values_of_array[compare_indx], values_of_array[compare_indx +
+        1]);
+      }
+    }
+  }
+}
 
+unsigned get_rand_in_range(unsigned small, unsigned biggest)
+{
+  auto seed = static_cast<unsigned int>(time(nullptr));
+  srand(seed);
+  unsigned value = rand() % (biggest - small + 1) + small;
+  return value;
+}
+
+void generate_the_array(int values_of_array[], unsigned
+user_allocated_size)
+{
+  int random;
   unsigned small;
   unsigned biggest;
   cout << "Enter the minimum value: ";
   cin >> small;
   cout << "Enter the minimum value: ";
   cin >> biggest;
+   for (unsigned looptimes = 0; looptimes < user_allocated_size; looptimes++)
+   {
+     random = get_rand_in_range(small, biggest);
+     values_of_array[looptimes] = random;
+   }
+}
 
-  for (unsigned looptimes = 0; looptimes < user_allocated_size; looptimes++ )
+void out_put_the_array(int values_of_array[], unsigned user_allocated_size)
+{
+  cout << "The values in order: [";
+  for (unsigned looptimes = 0; looptimes <= user_allocated_size; looptimes++)
   {
-    *values = get_rand_in_range(small, biggest);
-  }
-  cout << "generate " << user_allocated_size << " values" << endl;
-
-  sort_array(value_count);
-
-  cout << "The values in order: " << "[" << *values <<
-  "]" << endl;
-
-  copy_array_to_vector( values_vector, value_count);
-
-  bool done = false;
-  while (!done)
-  {
-    if ()
+    for (size_t position = 0; position < user_allocated_size; position++)
     {
-      binary_search_vector(values_vector);
+      cout << values_of_array[position] << ",";
     }
-    else
-    {
-      done = true;
-    }
-  } // while !done
-  return 0;
-}
-
-// function definitions here
-void sort_array(size_t value_count)
-{
-  for (size_t pass_indx = value_count - 1; pass_indx > 0; pass_indx--)
-  {
-    for (size_t compare_indx = 0; compare_indx < pass_indx; compare_indx++)
-    {
-      if (*values > values_array[compare_indx + 1])
-      {
-        swap(values_array[compare_indx], values_array[compare_indx + 1]);
-      }
-    }
+    cout << " ";
   }
-}
-
-void copy_array_to_vector( vector<int> &values_vector, unsigned value_count)
-{
-  for (unsigned copytimes = 0; copytimes < value_count; copytimes++)
-  {
-    values_vector.push_back(values_array[copytimes]);
-  }
-
-}
-
-size_t binary_search_vector(const vector<int> &values_vector)
-{
-  size_t first = 0;
-  size_t last = values_vector.size() - 1;
-  size_t position = values_vector.size();
-  bool found = false;
-  while (!found && first <= last && last < values_vector.size())
-  {
-    size_t middle = (first + last) / 2;
-    //找大小在这
-  }
-  return position;
-}
-
-size_t get_rand_in_range(unsigned low, unsigned high)
-{
-  auto seed = static_cast<unsigned int>(time(nullptr));
-  srand(seed);
-  unsigned value = rand() % (high - low + 1) + low;
-  return value;
-}
-
-size_t get_the_size_of_array()
-{
-  unsigned user_allocated_size;
-  cout << "Enter the number of values: ";
-  cin >> user_allocated_size;
-  int* values = new int[user_allocated_size];
-  return *values;
 }

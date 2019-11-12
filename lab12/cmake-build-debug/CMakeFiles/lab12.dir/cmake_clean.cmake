76eb0a7b6a316db518e170dc63f3dcc9597700d4
Miles Lee <1789216340lhy@gmail.com>
2019-11-12 12:41:21 -0600

    更新

---
+++ lab12/cmake-build-debug/CMakeFiles/lab12.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab12.dir/strings.cpp.o"
+  "lab12"
+  "lab12.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab12.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lab12/strings.cpp
@@ -0,0 +1,78 @@
+// Your Name Here
+// The program purpose here
+
+#include <cctype>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <cstring>
+#include <vector>
+using namespace std;
+
+/**
+ * break a string into three tokens at commas
+ * @param line the string containing tokens and commas
+ * @param tokens a vector that contains the tokens after they have been
+ * found
+ */
+void split(const char* line, vector<string>& tokens);
+
+
+int main(int argc, char* argv[])
+{
+  const unsigned ID_WIDTH = 8;
+  const unsigned COMMA_WIDTH = 1;
+  const unsigned NAME_WIDTH = 15;
+  const unsigned MAX_INPUT_LINE_LENGTH = ID_WIDTH + 2 * COMMA_WIDTH +
+                                         NAME_WIDTH;
+  const unsigned PAD_LENGTH = 5;
+  const unsigned OUTPUT_COLUMN_ONE = NAME_WIDTH + PAD_LENGTH;
+
+  if (argc != 2)
+  {
+    cout << "usage: " << argv[0] << " filename" << endl;
+    return 1;
+  }
+
+  ifstream data_file(argv[1]);
+  if (data_file.fail())
+  {
+    cout << "could not open data file " << argv[1] << endl;
+    return 2;
+  }
+
+  ofstream username_file("usernames.txt");
+  char line[MAX_INPUT_LINE_LENGTH];
+
+  while (data_file.getline(line, MAX_INPUT_LINE_LENGTH))
+  {
+    vector<string> fields;
+    split(line, fields);
+
+    string id = fields.at(0);
+    cout << id << endl;
+
+    // the rest of the code for the output here
+  }
+
+  data_file.close();
+  username_file.close();
+
+  return 0;
+}
+
+void split(const char* line, vector<string>& tokens)
+{
+  const char DELIMITER[] {','};
+
+  char* token_end = strstr(line, DELIMITER);
+  size_t number_of_characters = static_cast<size_t>(token_end - line);
+
+  string id;
+  for (size_t count = 0; count < number_of_characters; count++)
+  {
+    id += line[count];
+  }
+  tokens.push_back(id);
+
+}

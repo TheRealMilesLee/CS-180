f840a440ea0370036fd4bd10b7335088a0de1eb7
Miles Lee <1789216340lhy@gmail.com>
2020-04-20 19:27:20 -0500

    Update

--- 算法模版/sorting/bubble_sort.cpp
+++ 算法模版/sorting/bubble_sort.cpp
@@ -15,7 +15,7 @@ int main()
 void bubble_sort(vector<int> &array)
 {
     for(size_t pass_indx = array.size(); pass_indx > 0; pass_indx--)
-    {
+    {	
         for(size_t compare_indx = 0; compare_indx < pass_indx; compare_indx++)
         {
             if(array.at(compare_indx) > array.at(compare_indx + 1))

d2e7ad678d6ed791c2a61a8c8a61906db9963143
Miles Lee <1789216340lhy@gmail.com>
2020-04-29 10:11:51 -0500

    Update binary_search.cpp

--- 算法模版/searching/binary_search.cpp
+++ 算法模版/searching/binary_search.cpp
@@ -19,15 +19,13 @@ size_t binary_search(const vector<int> &array, size_t values)
     size_t last = array.size() - 1;
     size_t size = array.size();
     size_t position = size;
-    bool found = false;
 
-    while(!found && first < last && last < array.size())
+    while(first < last && last < array.size())
     {
         size_t middle = (first + last) / 2;
         if(array.at(middle) == values)
         {
-            found = true;
-            position = middle;
+          return middle;
         }
         else if(array.at(middle) > values)
         {
@@ -38,5 +36,5 @@ size_t binary_search(const vector<int> &array, size_t values)
             first = middle + 1;
         }    
     }
-    return position;
+    return -1;
 }
